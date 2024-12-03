d43f1197529c4d4940c734aca318cff0fa6d13f8
NahidePinarDemiriz <demiriznahidepinar@gmail.com>
2024-12-03 10:48:41 +0300

    The constructor_func.cpp and object_func.cpp files have been added.

--- section_11/constructor_func.cpp
+++ section_11/constructor_func.cpp
@@ -3,9 +3,30 @@
 
 using namespace std;
 
-int main()
-{
-    
-    
+class Book {
+    public:
+        string title;  // Attribute to store the book's title
+        string author; // Attribute to store the book's author
+        int pages;     // Attribute to store the number of pages
+
+        // Constructor declaration (inside the class)
+        Book(string aTitle, string aAuthor, int aPages); 
+};
+
+// Constructor definition outside the class
+Book::Book(string aTitle, string aAuthor, int aPages) {
+    title = aTitle;
+    author = aAuthor;
+    pages = aPages;
+}
+
+int main() {
+    // Creating objects using the constructor
+    Book book1("Harry Potter", "JK Rowling", 500); // First book object
+    Book book2("The Hobbit", "J.R.R. Tolkien", 310); // Second book object
+
+    // Output the title of the first book
+    cout << book1.title;
+
     return 0;
 }
